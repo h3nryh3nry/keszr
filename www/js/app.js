1205b0218204c9ae9bf9b1d0885a7a131962163a
Jacek Lebioda <jaceklebi@o2.pl>
2015-10-29 21:56:09 +0100

    Basic subpages #time 30m

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,38 @@
-angular.module('ionicApp', ['ionic'])
-  
- .controller('AppCtrl', function($scope, $ionicSideMenuDelegate) {
+var app = angular.module('ionicApp', ['ionic']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    // Allowed application states
+    $stateProvider
+        .state('index', {
+            url : '/',
+            templateUrl: 'templates/home.html'
+        })
+        .state('map', {
+            url : '/map',
+            templateUrl: 'templates/map.html'
+        })
+        .state('found', {
+            url : '/found',
+            templateUrl: 'templates/found.html'
+        })
+        .state('friends', {
+            url : '/friends',
+            templateUrl: 'templates/friends.html'
+        })
+        .state('go', {
+            url : '/go',
+            templateUrl: 'templates/go.html'
+        })
+        .state('settings', {
+            url : '/settings',
+            templateUrl: 'templates/settings.html'
+        });
+    
+    // When route does not match, go to '/'
+    $urlRouterProvider.otherwise('/');
+});
+
+var AppCtrl = app.controller('AppCtrl', function($scope, $ionicSideMenuDelegate) {
     
     $scope.toggleLeftMenu = function() {
         $ionicSideMenuDelegate.toggleLeft();
